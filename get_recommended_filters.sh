ab53e6fd2e4d1b32da99c2310c09a20679b64d41
Viruts1412 <144455283+Viruts1412@users.noreply.github.com>
2023-09-08 19:53:01 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -9,6 +9,7 @@ urls=(
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
   https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
   https://adaway.org/hosts.txt
+  https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 )
 
